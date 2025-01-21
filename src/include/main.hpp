149e72fd37ccff0d5a7b01059ef91b6012952265
issam eljaouhary <iseljao89@gmail.com>
2025-01-21 16:21:23 +0000

    webrtc init

--- src/include/main.hpp
+++ src/include/main.hpp
@@ -1,14 +1,6 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
 
-#include <opencv2/opencv.hpp>
 #include <iostream>
-#include <vector>
-#include <map>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <boost/asio.hpp>
-#include <string>
-#include "crow_all.h"
-#include "camera.hpp"
 
-std::string getCodec(const cv::VideoCapture *cap);
+#endif
